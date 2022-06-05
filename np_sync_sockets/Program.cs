388deb94edac7ac85447eaa1ee2198ad7b050ca7
vladtymo <tymo.vlad@gmail.com>
2022-06-05 16:54:24 +0300

    Add project files.

---
+++ np_sync_sockets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace np_sync_sockets
+{
+    class Program
+    {
+        static int port = 8080; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1"); //localhost
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+
+            // об'єкт для отримання адреси відправника
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    // получаем сообщение
+                    int bytes = 0;
+                    byte[] data = new byte[1024];
+                    bytes = listenSocket.ReceiveFrom(data, ref remoteEndPoint);
+
+                    string msg = Encoding.Unicode.GetString(data, 0, bytes);
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {msg} from {remoteEndPoint}");
+
+                    // отправляем ответ
+                    string message = "Message was send!";
+                    data = Encoding.Unicode.GetBytes(message);
+                    listenSocket.SendTo(data, remoteEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ sync_client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace sync_client
+{
+    class Program
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8080; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                // IP4 samples: 123.5.6.3    0.0.255.255    10.7.123.184
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                string message = "";
+                while (message != "end")
+                {
+                    Console.Write("Enter a message: ");
+                    message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+
+                    socket.SendTo(data, ipPoint);
+
+                    // при використанні UDP протоколу, Connect() лише встановлює дані для відправки
+                    //socket.Connect(ipPoint);
+                    //socket.Send(data);
+
+                    // получаем ответ получаем сообщение
+                    int bytes = 0;
+                    string response = "";
+                    data = new byte[1024]; // 1KB
+                    do
+                    {
+                        bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
+                        response += Encoding.Unicode.GetString(data, 0, bytes);
+                    } while (socket.Available > 0);
+
+                    Console.WriteLine("server response: " + response);
+                }
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
