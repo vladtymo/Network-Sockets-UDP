b225c049e99d71c9a5f065ea50519029cb3ec2a6
vladtymo <tymo.vlad@gmail.com>
2022-06-11 12:46:17 +0300

    Change Socket class to UDPClient.

--- np_sync_sockets/Program.cs
+++ np_sync_sockets/Program.cs
@@ -19,36 +19,44 @@ namespace np_sync_sockets
             IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
 
             // об'єкт для отримання адреси відправника
-            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
             // создаем сокет
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // связываем сокет с локальной точкой, по которой будем принимать данные
+            UdpClient server = new UdpClient(ipPoint);
+                
             try
             {
                 // связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(ipPoint);
+                //listenSocket.Bind(ipPoint);
                 Console.WriteLine("Server started! Waiting for connection...");
 
                 while (true)
                 {
                     // получаем сообщение
-                    int bytes = 0;
-                    byte[] data = new byte[1024];
-                    bytes = listenSocket.ReceiveFrom(data, ref remoteEndPoint);
+                    //int bytes = 0;
+                    //byte[] data = new byte[1024];
+                    //bytes = listenSocket.ReceiveFrom(data, ref remoteEndPoint);
+                    byte[] data = server.Receive(ref remoteEndPoint);
 
-                    string msg = Encoding.Unicode.GetString(data, 0, bytes);
+                    string msg = Encoding.Unicode.GetString(data, 0, data.Length);
                     Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {msg} from {remoteEndPoint}");
 
                     // отправляем ответ
                     string message = "Message was send!";
                     data = Encoding.Unicode.GetBytes(message);
-                    listenSocket.SendTo(data, remoteEndPoint);
+                    //listenSocket.SendTo(data, remoteEndPoint);
+                    server.Send(data, data.Length, remoteEndPoint);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+            server.Close();
         }
     }
 }
--- sync_client/Program.cs
+++ sync_client/Program.cs
@@ -16,10 +16,11 @@ namespace sync_client
             {
                 IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
 
-                EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+                IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
 
                 // IP4 samples: 123.5.6.3    0.0.255.255    10.7.123.184
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                UdpClient client = new UdpClient();
 
                 string message = "";
                 while (message != "end")
@@ -28,27 +29,31 @@ namespace sync_client
                     message = Console.ReadLine();
                     byte[] data = Encoding.Unicode.GetBytes(message);
 
-                    socket.SendTo(data, ipPoint);
+                    //socket.SendTo(data, ipPoint);
+                    client.Send(data, data.Length, ipPoint);
 
                     // при використанні UDP протоколу, Connect() лише встановлює дані для відправки
-                    //socket.Connect(ipPoint);
-                    //socket.Send(data);
+                    // socket.Connect(ipPoint);
+                    // socket.Send(data);
 
                     // получаем ответ получаем сообщение
-                    int bytes = 0;
-                    string response = "";
-                    data = new byte[1024]; // 1KB
-                    do
-                    {
-                        bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
-                        response += Encoding.Unicode.GetString(data, 0, bytes);
-                    } while (socket.Available > 0);
+                    //int bytes = 0;
+                    //string response = "";
+                    //data = new byte[1024]; // 1KB
+                    //do
+                    //{
+                    //    bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
+                    //    response += Encoding.Unicode.GetString(data, 0, bytes);
+                    //} while (socket.Available > 0);
+                    data = client.Receive(ref remoteIpPoint);
+                    string response = Encoding.Unicode.GetString(data, 0, data.Length);
 
                     Console.WriteLine("server response: " + response);
                 }
                 // закрываем сокет
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+                client.Close();
             }
             catch (Exception ex)
             {
